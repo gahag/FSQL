7b4747b7ce0419a3ecea87dc8eda66dabc7bae2f
gahag <gahag@hotmail.com.br>
2015-02-04 04:09:12 -0200

    Improved type error messages

--- src/Expr.hs
+++ src/Expr.hs
@@ -7,7 +7,8 @@
  - of the BSD license. See the LICENSE file for details.
  -}
 
-{-# LANGUAGE LambdaCase, TupleSections, RankNTypes #-}
+{-# LANGUAGE LambdaCase, TupleSections #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Expr (
     Expr(..), Atom(..), Value(..), Op(..),
@@ -55,15 +56,20 @@ module Expr (
   type TypeChecked = Either TypeError
   
   
+  instance Show Atom where
+    show = \case Sel s -> show s
+                 Val v -> show v
+  
   instance Show Value where
-    show (UnparsedVal s) = error "Expr.Value.show: unparsed value"
-    show (StrVal s)  = s
-    show (DayVal d)  = show d
-    show (SizeVal s) = show s
+    show = \case UnparsedVal s -> s
+                 StrVal s      -> s
+                 DayVal d      -> show d
+                 SizeVal s     -> show s
   
   instance Show TypeError where
-    show (InvalidValue u e) = concat ["invalid ", e, ": ", u]
-    show (UnexpectedType u e) = concat ["unexpected ", u, ", expected ", e]
+    show =
+      \case InvalidValue   u e -> concat ["invalid ", e, ": ", u]
+            UnexpectedType u e -> concat ["unexpected ", u, "\nexpected ", e]
   
   
   val     = Atom . Val
@@ -71,16 +77,16 @@ module Expr (
   dayVal  = val . DayVal
   sizeVal = val . SizeVal
   
-  quote s = "'" ++ show s ++ "'"  
+  quote s = "\"" ++ show s ++ "\""  
   invalid_val = InvalidValue . quote
   unexpected  = UnexpectedType
-  x `expected` s = throwError (x s)
+  x `expecting` s = throwError (x s)
   
   
   -- typecheck -----------------------------------------------------------------
   typecheck :: Expr -> TypeChecked Expr
   
-  typecheck (Atom _) = unexpected "atom" `expected` "expression"
+  typecheck (Atom a) = unexpected (quote a) `expecting` "expression"
   
   typecheck (Not x) = Not <$> typecheck x
   
@@ -96,39 +102,35 @@ module Expr (
                               GreatEq -> a'a'
                               Like    -> name'strVal'
     where
-      bool'bool' =
-        case (x, x') of
-          (Atom _, _) -> unexpected "atom" `expected` "expreesion"
-          (_, Atom _) -> unexpected "atom" `expected` "expreesion"
-          _           -> (,) <$> typecheck x <*> typecheck x'
+      bool'bool' = (,) <$> typecheck x <*> typecheck x'
       
       name'strVal'
         | (Atom a, Atom a') <- (x, x')
           = case (a, a') of
               (Sel Name, Val (UnparsedVal s)) -> return (x, strVal s)
-              (Sel s, _) -> unexpected (quote s) `expected` "name"
-              (Val v, _) -> unexpected (quote v) `expected` "name"
-        | otherwise = unexpected "expression" `expected` "atom"
+              (Sel Name, a) -> unexpected (quote a) `expecting` "value"
+              (a, _) -> unexpected (quote a) `expecting` "`name`"
+        | otherwise = unexpected "expression" `expecting` "selection or value"
       
       a'a'
         | (Atom a, Atom a') <- (x, x')
           = case (a, a') of
               (Sel s, Val v) -> (x,)  <$> parseVal s v
               (Val v, Sel s) -> (,x') <$> parseVal s v
-              (Sel _, Sel _) -> unexpected "selection" `expected` "value"
-              (Val _, Val _) -> unexpected "value" `expected` "selection"
-        | otherwise = unexpected "expression" `expected` "atom"
+              (Sel _, a) -> unexpected (quote a) `expecting` "value"
+              (Val _, a) -> unexpected (quote a) `expecting` "selection"
+        | otherwise = unexpected "expression" `expecting` "selection or value"
         where
           parseVal sel (UnparsedVal str) =
             let parse f   = maybe parse_err (return . f) . readMaybe
-                parse_err = invalid_val str `expected` (show sel ++ " value")
+                parse_err = invalid_val str `expecting` (show sel ++ " value")
             in
               case sel of
-                Name -> parse strVal  str
+                Name -> return (strVal str)
                 Date -> parse dayVal  str
                 Size -> parse sizeVal str
           
-          parseVal _ _ = error "Expr.typecheck.parseVal: not `UnparsedVal`"
+          parseVal _ _ = error "Expr.typecheck.parseVal: not UnparsedVal"
   -- ---------------------------------------------------------------------------
   
   
